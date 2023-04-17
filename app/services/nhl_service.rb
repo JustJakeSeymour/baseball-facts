fc69ab4769fba78b70e4b9d3e81f353fdeca52da
J Seymour <JustJakeSeymour@gmail.com>
2023-04-17 13:46:30 -0600

    make facade and poro

--- app/services/nhl_service.rb
+++ app/services/nhl_service.rb
@@ -1,6 +1,11 @@
 class NhlService
-  def self.game_data(team, date)
+  def self.game_overview(team, date)
     response = conn.get("schedule?teamId=#{team}&date=#{date}")
+    parse_json(response)
+  end
+
+  def self.game_details(game_id)
+    response = conn.get("game/#{game_id}/boxscore")  
     parse_json(response)
   end
 
--- spec/services/nhl_service_spec.rb
+++ spec/services/nhl_service_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe NhlService, type: :service do
     it 'game_data', :vcr do
       team = '21'
       date = '2023-04-11'
-      service = NhlService.game_data(team, date)
-      
+      service = NhlService.game_overview(team, date)
+
       expect(service).to be_a(Hash)
       expect(service).to have_key(:dates)
     end
