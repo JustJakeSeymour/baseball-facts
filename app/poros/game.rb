7c77df87f4b38b263347ea9e31528f0366f7eb45
Joe King <j.king92@gmail.com>
2023-04-17 13:37:07 -0700

    chore: Add method for getting officials from game and hometeam.

--- app/poros/game.rb
+++ app/poros/game.rb
@@ -3,7 +3,18 @@ class Game
               :overview
 
   def initialize(overview, details)
+    require 'pry'; binding.pry
     @overview = overview
     @details = details
+  end
+
+  def home_team
+    overview[:dates][0][:games][0][:teams][:home][:team][:name]
+  end
+
+  def officials
+    details[:officials].map do |off|
+      off[:official][:fullName]
+    end.to_sentence
   end
 end
